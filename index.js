a2ae8363d05b4f5801a8ed36763952496ee203e9
AkshatGupta2005 <akshatguptaip@gmail.com>
2024-11-22 19:58:46 +0530

    login part complete

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import bodyParser from "body-parser";
 import pg from "pg";
 import fs from "fs";
 import dotenv from "dotenv";
+import session from "express-session";
+import RedisStore from "connect-redis";
+import Redis from "ioredis";
+import * as EmailValidator from "email-validator";
 dotenv.config();
 const app = express();
 const port = process.env.s_port;
@@ -30,12 +34,115 @@ db.query("SELECT * FROM users", (err, res) => {
 });
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+const client = new Redis({
+  password: "secret",
 });
-app.get("/login", (req, res) => {
+const store = new RedisStore({ client });
+
+app.use(
+  session({
+    store,
+    name: "sid",
+    saveUninitialized: false,
+    resave: false,
+    secret: "secret",
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+      sameSite: true,
+      secure: false, //change before prod
+    },
+  })
+);
+const emailVerification = async (email) => {
+  const valid = await EmailValidator.validate(email);
+  return valid;
+};
+const checkEmailExist = async (email) => {
+  const result = await db.query(
+    "SELECT EXISTS(SELECT 1 FROM users WHERE email = $1);",
+    [email]
+  );
+  return result.rows[0].exists;
+};
+const redirectHome = (req, res, next) => {
+  if (req.session.userid) {
+    res.redirect("/");
+  } else {
+    next();
+  }
+};
+const redirectLogin = (req, res, next) => {
+  if (!req.session.userid) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+};
+const updateNavbar = (req, res, next) => {
+  if (req.session.userid) {
+    res.render("navbar.ejs", { userLogin: true });
+    next();
+  } else {
+    next();
+  }
+};
+app.get("/", (req, res) => {
+  //console.log(req.session);
+  //console.log(req.session.userid);
+  updateNavbar;
+  res.render("index.ejs", { userid: req.session.userid });
+});
+app.get("/login", redirectHome, (req, res) => {
   res.render("login.ejs");
 });
+app.get("/userLogin", redirectHome, (req, res) => {
+  updateNavbar;
+  res.redirect("/");
+});
+app.post("/register", redirectHome, async (req, res) => {
+  var { name, email, pass } = req.body;
+  email = email.toLowerCase();
+  const result = await emailVerification(email);
+  if (result) {
+    const checkEmail = await checkEmailExist(email);
+    if (!checkEmail) {
+      const id = await db.query(
+        "INSERT INTO users(name,email,password) values($1,$2,$3) returning id;",
+        [name, email, pass]
+      );
+      req.session.userid = id.rows[0].id;
+      updateNavbar;
+      res.redirect("/");
+    } else {
+      const err = "Email Already Exist";
+      res.render("login.ejs", { err: err });
+    }
+  } else {
+    const err = "Invalid Email Id";
+    res.render("login.ejs", { err: err });
+  }
+});
+app.post("/logUser", async (req, res) => {
+  const { email, pass } = req.body;
+  const checkEmail = await checkEmailExist(email);
+  if (!checkEmail) {
+    const err = "Email Id Does Not Exist";
+    res.render("login.ejs", { err: err });
+  } else {
+    const id = await db.query(
+      "SELECT id FROM users WHERE email = $1 AND password = $2",
+      [email, pass]
+    );
+    req.session.userid = id.rows[0].id;
+    updateNavbar;
+    res.redirect("/");
+  }
+});
+
+app.get("/profile", redirectLogin, (req, res) => {
+  res.render("profile.ejs", { userid: req.session.userid });
+});
+
 app.listen(port, () => {
   console.log(`Server active on port : ${port}`);
 });
